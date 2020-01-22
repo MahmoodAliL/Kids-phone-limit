3857e2e36cfce0414d1abb5682705853f0a1e2fa
Mahmood_L <mahmood07700998512@gmail.com>
2020-01-23 00:51:16 +0300

    init fragments

---
+++ app/src/main/java/com/teaml/kidsphonelimit/ui/home/HomeFragment.kt
@@ -0,0 +1,50 @@
+package com.teaml.kidsphonelimit.ui.home
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+import com.teaml.kidsphonelimit.R
+import kotlinx.android.synthetic.main.home_fragment.*
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? AppCompatActivity)?.setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+}
---
+++ app/src/main/java/com/teaml/kidsphonelimit/ui/lock/LockViewModel.java
@@ -0,0 +1,7 @@
+package com.teaml.kidsphonelimit.ui.lock;
+
+import androidx.lifecycle.ViewModel;
+
+public class LockViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
