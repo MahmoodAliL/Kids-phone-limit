8efd4be7c02c7fdc1b91185a09d9d6e893cff124
Mahmood_L <mahmood07700998512@gmail.com>
2020-01-25 12:49:03 +0300

    Code Review 1

--- app/src/main/java/com/teaml/kidsphonelimit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/teaml/kidsphonelimit/ui/home/HomeFragment.kt
@@ -4,18 +4,15 @@ import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
 import android.view.*
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.teaml.circulartimerview.CircularTimerListener
 import com.teaml.circulartimerview.TimeFormatEnum
 
 import com.teaml.kidsphonelimit.R
-import kotlinx.android.synthetic.main.home_fragment.*
+import com.teaml.kidsphonelimit.databinding.HomeFragmentBinding
 import java.text.DecimalFormat
 import kotlin.math.ceil
-import kotlin.math.floor
-import kotlin.math.round
 
 class HomeFragment : Fragment() {
 
@@ -25,10 +22,14 @@ class HomeFragment : Fragment() {
 
     private lateinit var viewModel: HomeViewModel
 
+    private var _binding: HomeFragmentBinding? = null
+    private val binding get() = _binding!!
+    
+    private var timeSelected = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-
 
     }
 
@@ -36,53 +37,92 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+        _binding = HomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (activity as? AppCompatActivity)?.setSupportActionBar(toolbar)
 
-        minute_picker.setFormatter { value ->
-            String.format("%02d", value)
+        setSupportActionBar()
+        setupMinutePicker()
+        setupCircularTimerView()
+        
+        binding.startTimerBtn.setOnClickListener { 
+            startTimer(timeSelected)
+            binding.minutePickerLayout.visibility = View.GONE
+            binding.timerProgress.visibility = View.VISIBLE
         }
 
-        timer_progress.progress = 0F
-        timer_progress.setCircularTimerListener(object : CircularTimerListener {
-            override fun updateDataOnTick(remainingTimeInMs: Long): String? {
-                val decimalFormat = DecimalFormat("00")
-                val second = ceil(remainingTimeInMs.div(1_000f)).toInt()
-                Log.e("timer MS", remainingTimeInMs.toString())
-                Log.e("timer SE" , second.toString())
-                val min = decimalFormat.format(second / 60)
-                val sec = decimalFormat.format(second % 60)
-
-                return "$min:$sec"
-            }
-
-            override fun onTimerFinished() {
-                Toast.makeText(context, "onTimerFinished", Toast.LENGTH_LONG).show()
-            }
-        }, 1, TimeFormatEnum.MINUTES,10)
-        timer_progress.startTimer()
-
-        /*   recycler_view.adapter = MinutesAdapter()
-           recycler_view.layoutManager = LinearLayoutManager(context)*/
     }
 
-    private fun setTime() {
+    private fun setSupportActionBar() {
+        (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
+    }
 
+    private fun setupMinutePicker() {
+        binding.minutePicker.setFormatter { value ->
+            String.format("%02d", value)
+        }
+        
+        binding.minutePicker.setOnValueChangedListener { _, _, newVal -> 
+            timeSelected = newVal
+        }
+    }
+
+    private fun setupCircularTimerView() {
+        binding.timerProgress.progress = 0f
+    }
+
+
+    private fun startTimer(minute: Int) {
+
+        with(binding) {
+
+            timerProgress.progress = 0f
+            timerProgress.setCircularTimerListener(
+                CircularTimerListener1(), minute.toLong(),
+                TimeFormatEnum.MINUTES, 1000
+            )
+
+            timerProgress.startTimer()
+        }
+        
+    }
+
+    inner class CircularTimerListener1 : CircularTimerListener {
+        override fun updateDataOnTick(remainingTimeInMs: Long): String? {
+            val decimalFormat = DecimalFormat("00")
+            val second = ceil(remainingTimeInMs.div(1_000f)).toInt()
+            Log.e("timer MS", remainingTimeInMs.toString())
+            Log.e("timer SE", second.toString())
+            val min = decimalFormat.format(second / 60)
+            val sec = decimalFormat.format(second % 60)
+
+            return "$min:$sec"
+        }
+
+        override fun onTimerFinished() {
+            //Toast.makeText(context, "onTimerFinished", Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.home_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 
 
