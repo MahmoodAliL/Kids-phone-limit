2a4596287452f5b4a29398493d7d3de76938829a
Ali <ali00.hasan00@gmail.com>
2020-01-25 08:51:55 +0300

    Add circlartimerview to home layout

--- app/src/main/java/com/teaml/kidsphonelimit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/teaml/kidsphonelimit/ui/home/HomeFragment.kt
@@ -2,15 +2,20 @@ package com.teaml.kidsphonelimit.ui.home
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import android.view.*
-import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.teaml.circulartimerview.CircularTimerListener
+import com.teaml.circulartimerview.TimeFormatEnum
 
 import com.teaml.kidsphonelimit.R
-import com.teaml.kidsphonelimit.ui.home.adapter.MinutesAdapter
 import kotlinx.android.synthetic.main.home_fragment.*
+import java.text.DecimalFormat
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.round
 
 class HomeFragment : Fragment() {
 
@@ -31,7 +36,6 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
@@ -49,8 +53,31 @@ class HomeFragment : Fragment() {
             String.format("%02d", value)
         }
 
+        timer_progress.progress = 0F
+        timer_progress.setCircularTimerListener(object : CircularTimerListener {
+            override fun updateDataOnTick(remainingTimeInMs: Long): String? {
+                val decimalFormat = DecimalFormat("00")
+                val second = ceil(remainingTimeInMs.div(1_000f)).toInt()
+                Log.e("timer MS", remainingTimeInMs.toString())
+                Log.e("timer SE" , second.toString())
+                val min = decimalFormat.format(second / 60)
+                val sec = decimalFormat.format(second % 60)
+
+                return "$min:$sec"
+            }
+
+            override fun onTimerFinished() {
+                Toast.makeText(context, "onTimerFinished", Toast.LENGTH_LONG).show()
+            }
+        }, 1, TimeFormatEnum.MINUTES,10)
+        timer_progress.startTimer()
+
         /*   recycler_view.adapter = MinutesAdapter()
            recycler_view.layoutManager = LinearLayoutManager(context)*/
+    }
+
+    private fun setTime() {
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
